38d7544f6a54d42ed394c956bdc2c27961a9867e
Al Mehedi <leftoverm9@gmail.com>
2025-04-17 08:52:00 +0600

    adding build step for always execute from zig-out/bin dir

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,4 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-
-const cfp = @import("cfp");
-const Cfp = cfp.Cfp;
 
 pub fn main() !void {
     std.debug.print("Hello, World!\n", .{});
